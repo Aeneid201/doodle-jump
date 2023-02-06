11ad24303feee9977ebbba23ffeeea772ab8efa9
Aeneid201 <aloubi.oumaima@gmail.com>
2023-02-06 16:58:36 -0500

    second commit

--- script.js
+++ script.js
@@ -4,9 +4,11 @@
 const grid = document.querySelector(".grid");
 const doodler = document.createElement("div");
 let doodlerLeftSpace = 50;
-let doodlerBottomSpace = 150;
+let doodlerBottomSpace = 210;
 let isGameOver = false;
 let platformCount = 5;
+let platforms = [];
+let downTimerId, upTimerId;
 
 // Create platforms
 function createPlatforms() {
@@ -14,22 +16,39 @@ function createPlatforms() {
     let platGap = 600 / platformCount;
     let newPlatBottom = 100 + i * platGap;
     let newPlatform = platform(newPlatBottom);
+    platforms.push(newPlatform);
+    console.log(platforms);
   }
 }
 
+// Create Platform
 function platform(newPlatBottom) {
-  bottom = newPlatBottom;
-  left = Math.random() * 315;
-  visual = document.createElement("div");
-  const visual = this.visual;
+  let bottom = newPlatBottom;
+  let left = Math.random() * 315;
+  let visual = document.createElement("div");
   visual.classList.add("platform");
   visual.style.left = left + "px";
-  visual.style.bottom = this.bottom + "px";
+  visual.style.bottom = bottom + "px";
   grid.appendChild(visual);
 
   return { bottom, left, visual };
 }
 
+// Move platform
+function movePlatforms() {
+  if (doodlerBottomSpace > 200) {
+    platforms.forEach((platform) => {
+      platform.bottom -= 4;
+      let visual = platform.visual;
+      visual.style.bottom = platform.bottom + "px";
+    });
+  }
+}
+
+// Interval move platforms
+setInterval(movePlatforms, 30);
+
+// Create Doodler
 function createDoodler() {
   grid.appendChild(doodler);
   doodler.classList.add("doodler");
@@ -37,10 +56,34 @@ function createDoodler() {
   doodler.style.bottom = doodlerBottomSpace + "px";
 }
 
+// Jump function
+function jump() {
+  clearInterval(downTimerId);
+  upTimerId = setInterval(function () {
+    doodlerBottomSpace += 20;
+    doodler.style.bottom = doodlerBottomSpace + "px";
+    if (doodlerBottomSpace > 450) {
+      fall();
+    }
+  }, 30);
+}
+
+function fall() {
+  clearInterval(upTimerId);
+  downTimerId = setInterval(function () {
+    doodlerBottomSpace -= 5;
+    doodler.style.bottom = doodlerBottomSpace + "px";
+    if (doodlerBottomSpace < 20) {
+      jump();
+    }
+  }, 30);
+}
+
 function start() {
   if (!isGameOver) {
     createDoodler();
     createPlatforms();
+    jump();
   }
 }
 
